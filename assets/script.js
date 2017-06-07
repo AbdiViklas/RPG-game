7ee29bbceb6280765357c1905a1348f0168cd1b4
Andy Bonner <abonner@duke.edu>
2017-06-07 14:02:50 -0400

    reorganize file tree

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+document
+
+/*theme ideas:
+geeky obvious:
+  dr. who
+  lotr
+  generic Tolien-esque RPG
+  space
+  Office Space style
+  musicians
+  tech theme--browsers? OSs?
+  Greek mythology
+  famous authors, musicians, etc. (e.g. Tolstoy vs Hemingway vs Jane Austen, or Beethoven vs Mahler vs Stravinsky...)
+non-violent?
+  two entities are starting with a set amount of X apiece and depleting each other's X...
+  non-violent but combative: water balloons? & darts?
+  helium balloons and darts!
+  each combatant has an amount of chocolate that the other eats
+  positive model: X is a person's "problems", and when two entities engage they deplete each other's problems
+    But then the goal gets weird. When you engage with someone and they deplete all your problems wouldn't that be a good thing? Would the goal of this game be to be the only person left with problems?
+  
+*/
