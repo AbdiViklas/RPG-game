4febc0a0e9b200709edb08a2f0739852590eb080
Andy Bonner <abonner@duke.edu>
2017-06-07 21:23:02 -0400

    create character objects

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,93 @@
-document
+// CHARACTER OBJECTS
+var dr10 = {
+  name: "the Tenth Doctor",
+  maxHealth: 80,
+  baseAttack: 5,
+  attacks: [
+    "sonic screwdriver",
+    "Plimsolls", //check sp
+    "spiky hair",
+    "Run!"
+  ],
+  phrases: [
+    "Allons-y!",
+    "Run!"
+  ]
+}
 
-/*theme ideas:
-characters:
-  doctors 1-12?
-  or just the highlights--10, 11, 12, the scarf one, number 1?
-  dalek
-  cyberman
-  Master and/or Missie?
-  companions? (Pond, Rose, Martha Jones, Clara Oswold)
-  Shadow?
-  weeping Angels
+var dr11 = {
+  name: "the Eleventh Doctor",
+  maxHealth: 90,
+  baseAttack: 7,
+  attacks: [
+    "sonic screwdriver",
+    "fez"
+  ],
+  phrases: [
+    "Bowties are cool.",
+    "A good man goes to war!",
+    "I am definitely a madman with a box!"
+  ]
+}
 
-*/
+var dalek = {
+  name: "a Dalek",
+  maxHealth: 30,
+  baseAttack: 10,
+  attacks: [
+    "scream",
+    "plunger",
+    "laser"
+  ],
+  phrases: [
+    "Exterminate! EXTERMINATE!",
+    "You are a good Dalek!"
+  ]
+}
+
+var cyberman = {
+  name: "a Cyberman",
+  maxHealth: 50,
+  baseAttack: 8,
+  attacks: [
+    "wrist missiles",
+    "fly"
+  ],
+  phrases: [
+    ""
+  ]
+}
+
+var river = {
+  name: "River Song",
+  maxHealth: 40,
+  baseAttack: 5,
+  attacks: [
+    "notebook",
+    "sonic screwdriver"
+  ],
+  phrases: [
+    "Spoilers, sweetie!"
+  ]
+}
+
+var amy = {
+  name: "Amy Pond",
+  maxHealth: 60,
+  baseAttack: 7,
+  attacks: [
+    "wait",
+    "tell off"
+  ],
+  phrases: [
+    ""
+  ]
+}
+
+// play starts when user chooses a character (by clicking)
+// start with a tutorial-style modal instructing that
+var userChar;
+
+function chooseChar(params) {
+  
+}
